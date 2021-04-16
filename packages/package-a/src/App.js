b5a157301d30658154d8ba5367ac5bc2eaf35d7e
João Cháchá <joaorodrigues.xp.15@gmail.com>
2021-04-16 13:05:16 +0100

    feat(package-a): update main description

--- packages/package-a/src/App.js
+++ packages/package-a/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <code>package-a</code>. Feel free to edit and test.
         </p>
         <a
           className="App-link"
